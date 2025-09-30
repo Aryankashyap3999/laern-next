6f692931a8cf907793972da835cc7913239c6683
Aryan Kashyap <aryankashyapbanka2003@gmail.com>
2025-10-01 00:37:55 +0530

    Added fetching api's in next

--- src/app/FirstServerComponent.tsx
+++ src/app/FirstServerComponent.tsx
@@ -1,11 +1,29 @@
 import ClientButton from "./ClientButton";
 
-export default function FirstServerComponent () {
+const getProducts = async () => {
+  const response = await fetch('https://fakestoreapi.com/products');
+  const content = await response.json();
+  return content;
+}
+
+interface Product {
+  id: string;
+  title: string;
+  price: string;
+  description: string;
+}
+
+
+export default async function FirstServerComponent () {
+    const products = await getProducts();
     return (
         <div>
             Welcome to first server component of next js project
             <br/>
             <ClientButton />
+            <div>
+                {products.map((product: Product) => <div key={product.id}>{product.title}</div>)}
+            </div>
         </div>
     )
 }
---
+++ src/app/blogs/layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function BlogLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div>
+      <h1>Blog Layout</h1>
+      <div>
+        {children}
+      </div>
+    </div>
+  );
+}
